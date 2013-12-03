8917ba3b1450f4957f4f211630f0506d57209e4e
Robert Badgett <rbadgett@kumc.edu>
2013-12-03 01:09:30 -0600

    Updates
    First working version

--- R/pulmnodule.R
+++ R/pulmnodule.R
@@ -1,8 +1,14 @@
-pulmnodule <- function(myname = ""){
-  if(myname == ""){
-    stop("Tell me your name!")
+pulmnodule <- function(age = 0, diameter = 0, cancer = 0, smoker = 0, spiculation = 0, location = 0){
+  if(age < 1){
+    stop("Tell me your age!")
   }
+  prob = eval(-6.8272 + (age * 0.0391) + (diameter * 0.1274) + (cancer * 1.3388) + (smoker * 0.7917) + (location * 0.7838) + (1.0407 * spiculation))
+  prob = round(100 * exp(prob)/(1 + exp(prob)),1)
+  citations = "1. Swensen SJ, Silverstein MD, Ilstrup DM, Schleck CD, Edell ES: The probability of malignancy in solitary pulmonary nodules. Application to small radiologically indeterminate nodules. Arch Intern Med 157. (8): 849-855.1997;"
+  citations = paste(citations,"2. Herder GJ, van Tinteren H, Golding RP, et al: Clinical prediction model to characterize pulmonary nodules: validation and added value of 18F-fluorodeoxyglucose positron emission tomography. Chest 128. (4): 2490-2496.2005; Full Text ")
   list(
-    message = paste("hello", myname, "!")
+    message = paste(prob, '% probability of cancer')
   )
+#Swensen SJ, Silverstein MD, Ilstrup DM, Schleck CD, Edell ES: The probability of malignancy in solitary pulmonary nodules. Application to small radiologically indeterminate nodules. Arch Intern Med 157. (8): 849-855.1997;
+# Herder GJ, van Tinteren H, Golding RP, et al: Clinical prediction model to characterize pulmonary nodules: validation and added value of 18F-fluorodeoxyglucose positron emission tomography. Chest 128. (4): 2490-2496.2005; Full Text 
 }
