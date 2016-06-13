fc4ae370cd331e14cf1137150c0ffeb30aacbac2
Robert Badgett <rbadgett@kumc.edu>
2016-06-12 20:27:15 -0500

    First draft ready

--- R/prostatecancerrisk.R
+++ R/prostatecancerrisk.R
@@ -1,9 +1,5 @@
 prostatecancerrisk <- function(fmh = 'no', race = 'w', pageformat = 'factsbox'){
 
-fmh = 'n'
-race = 'w'
-prob = 0
-
 if (race == "w"){prob = 0.0258}
 if (race == "b"){prob = 0.0443}
 if (race == "h"){prob = 0.0314}
@@ -16,13 +12,14 @@ msg = NULL
 
 # https://github.com/jeroenooms/opencpu/issues/162
 factsbox <- system.file("www/factsbox-psa.html", package = "home")
+#stop(factsbox)
 nc <- nchar(factsbox)
-readChar(factsbox,nc)
+msg <- readChar(factsbox,10000)
 
-prop = prob * 100
+prob = prob * 100
 msg <- sub("XCONTROLX", sprintf("%.1f",prob),msg)
 
-prop = prob * 0.78
+prob = prob * 0.78
 msg <- sub("XPSAX", sprintf("%.1f",prob),msg)
 
 msg <- paste(msg,"<div>&nbsp;</div><div style=\"text-align:center\">		<button id=\"startover\" type=\"button\" onclick=\"location.reload()\">Start over</button></div>", sep="")
