30885c89d0dc89c6c6c2e52eb8c209bb887392ad
Robert Badgett <rbadgett@kumc.edu>
2018-02-22 05:34:18 -0600

    Holy Family

--- R/cardiovascularrisk.R
+++ R/cardiovascularrisk.R
@@ -324,7 +324,7 @@ if (pageformat == "chart")
 	if (nchar(site) > 1 && site == "holyfamily")
 		{
 		msg = paste(msg, "<h3>Additional recommendations:</h3><ul>")
-		msg = paste(msg, "<div>Welcome, ", site," diabetes = ", diabetes0," bmi = ", bmi, "</div>")
+		msg = paste(msg, "<div>For patients of Holy Family Medical Clinic.</div>")
 		if (diabetes0 == 2){msg = paste(msg, "<div>Low carbohydrate diet (no more than 50 grams) since you have prediabetes</div>")}
 		if (bmi >= 30){msg = paste(msg, "<div>Weight loss of 10% since you are obese. </div>")}
 		}  
