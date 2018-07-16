f4e5ab7dcefa06e42be739945c2836cac6905b72
Robert Badgett <rbadgett@kumc.edu>
2018-07-15 22:25:03 -0500

    testing

--- R/bcra.R
+++ R/bcra.R
@@ -11,6 +11,7 @@ bcra <- function(genetics = 'no', history = 'no', T1 = 0, AgeMen = 99, Age1st = 
     Race = as.numeric(Race)
   )
 
+  stop(myframe)
   
   x.sub <- subset(myframe, myframe[] == 999)
   
@@ -40,24 +41,24 @@ Avg_White <- 1
 data(pages)
 msg <- subset(pages, pagename==pageformat)[, "content"]
 
-#msg = paste(msg,'<div>5 years: ', sprintf("%.1f",absolute.risk(myframe, Raw_Ind=1, Avg_White)), '%.</div>')
+#msg = paste(msg,'<div>5 years: ', sprintf("%.1f",absolute.risk(myframee)), '%.</div>')
 if (myframe$T1 < 86){
   #5 years
   myframe$T2 = myframe$T1 + 5
-  msg <- sub("X5X", sprintf("%.1f",absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
-  msg <- sub("M5M", sprintf("%.1f",0.8*absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
+  msg <- sub("X5X", sprintf("%.1f",absolute.risk(myframee)),msg)
+  msg <- sub("M5M", sprintf("%.1f",0.8*absolute.risk(myframee)),msg)
   msg <- sub("T5T", sprintf("%.1f",absolute.risk(myframe,iloop=2)),msg)
 }
 if (myframe$T1 < 81){
   #10 years
   myframe$T2 = myframe$T1 + 10
-  msg <- sub("X10X", sprintf("%.1f",absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
-  msg <- sub("M10M", sprintf("%.1f",0.8*absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
+  msg <- sub("X10X", sprintf("%.1f",absolute.risk(myframee)),msg)
+  msg <- sub("M10M", sprintf("%.1f",0.8*absolute.risk(myframee)),msg)
 }
 if (myframe$T1 < 91){
   myframe$T2 = 90
-  msg <- sub("X90X", sprintf("%.1f",absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
-  msg <- sub("M90M", sprintf("%.1f",0.8*absolute.risk(myframe, Raw_Ind=1, Avg_White)),msg)
+  msg <- sub("X90X", sprintf("%.1f",absolute.risk(myframee)),msg)
+  msg <- sub("M90M", sprintf("%.1f",0.8*absolute.risk(myframee)),msg)
   msg <- sub("T90T", sprintf("%.1f",absolute.risk(myframe,iloop=2)),msg)
 }
 msg <- paste(msg,"<div>&nbsp;</div><div style=\"text-align:center\">		<button id=\"startover\" type=\"button\" onclick=\"location.reload()\">Start over</button></div>", sep="")
