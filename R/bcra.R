45db4b33e48686b15b18c50bb842033e1bbfacc6
Robert Badgett <rbadgett@kumc.edu>
2018-11-08 08:23:34 -0600

    Update bcra.R

--- R/bcra.R
+++ R/bcra.R
@@ -11,7 +11,8 @@ bcra <- function(genetics = 'no', history = 'no', T1 = 0, AgeMen = 99, Age1st = 
     Race = as.numeric(Race)
   )
 
-stop(packageDescription("BCRA")) #.
+#Test for package availability
+#stop(packageDescription("BCRA")) #.
   
   x.sub <- subset(myframe, myframe[] == 999)
   
