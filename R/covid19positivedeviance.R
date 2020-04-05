2c19aa45b9df6d01619629b20ccc4674b384bd12
Robert Badgett <rbadgett@kumc.edu>
2020-04-05 18:26:55 -0500

    working copy

--- R/covid19positivedeviance.R
+++ R/covid19positivedeviance.R
@@ -1,45 +1,37 @@
-covid19positivedeviance <- function(age = 0, diameter = 0, cancer = 0, smoker = 0, spiculation = 0, location = 0, geo_region = 'none'){
-  if(age < 1){
-    stop("Tell me your age!")
-  }
-  message = '';
-  prob = eval(-6.8272 + (age * 0.0391) + (diameter * 0.1274) + (cancer * 1.3388) + (smoker * 0.7917) + (location * 0.7838) + (1.0407 * spiculation))
-  prob = round(100 * exp(prob)/(1 + exp(prob)),1)
-  citations = "1. Swensen SJ, Silverstein MD, Ilstrup DM, Schleck CD, Edell ES: The probability of malignancy in solitary pulmonary nodules. Application to small radiologically indeterminate nodules. Arch Intern Med 157. (8): 849-855.1997;"
-  citations = paste(citations,"2. Herder GJ, van Tinteren H, Golding RP, et al: Clinical prediction model to characterize pulmonary nodules: validation and added value of 18F-fluorodeoxyglucose positron emission tomography. Chest 128. (4): 2490-2496.2005; Full Text ")
-	message = paste(message,'<div><span class=\'emphasis\'>', prob, '%</span> probability of cancer. For next step, consider ');
-	if (prob >= 5){
-		message = paste(message, '<style>.emphasis{color:red;font-weight:bold}</style>');
-		};
-	#Messages based on probability
-	if (prob > 65){
-		message = paste(message,'both:</div><ul>');
-		message = paste(message,'<li><span class=\'emphasis\'>PET scan</span> as probability is high (ACCP 2012 rec 4.2.4.1)');
-		message = paste(message,'<li><span class=\'emphasis\'>Surgical diagnosis</span> as probability is high (ACCP 2012 rec 4.6.3.1.1)');
-		if (geo_region == "KS"){message = paste(message,'<ul><li><a href=\'http://www.mssconline.org/physician_finder.php?hdnScrollLeft=hdnScrollLeft&amp;hdnScrollTop=0&amp;rowcount=100&amp;ddlspecialty=Thoracic+Surgery\'>List of thoracic surgeons in Wichita region</a> (from the Medical Society of Sedgwick County)</li></ul>');}
-		message = paste(message,'</li>');
-		};
-	if (prob >= 5 && prob <= 65){
-		message = paste(message,'</div><ul>');
-		message = paste(message,'<li><span class=\'emphasis\'>PET scan</span> as probability is low to moderate (ACCP 2012 rec 4.2.4.1)');
-		if (geo_region == "KS"){message = paste(message,'<ul><li>If negative, consider bronchoscopic diagnosis. <a href=\'http://www.mssconline.org/physician_finder.php?hdnScrollLeft=hdnScrollLeft&amp;hdnScrollTop=0&amp;rowcount=100&amp;ddlspecialty=Pulmonary+Disease\'>List of pulmonologists in Wichita region</a>.</li><li>If positive, consider surgical diagnosis. <a href=\'http://www.mssconline.org/physician_finder.php?hdnScrollLeft=hdnScrollLeft&amp;hdnScrollTop=0&amp;rowcount=100&amp;ddlspecialty=Thoracic+Surgery\'>List of thoracic surgeons in Wichita region</a>.</li></ul>');}
-		message = paste(message,'</li>');
-		};
-	if (prob < 5){
-		message = paste(message,'</div><ul>');
-		message = paste(message,'<li><span class=\'emphasis\'>CT surveillance</span> as probability is very low (ACCP 2012 rec 4.5.1.1)');
-		if (geo_region == "KS"){message = paste(message,'<ul><li>Consider pulmonary consultation. <a href=\'http://www.mssconline.org/physician_finder.php?hdnScrollLeft=hdnScrollLeft&amp;hdnScrollTop=0&amp;rowcount=100&amp;ddlspecialty=Pulmonary+Disease\'>List of pulmonologists in Wichita region</a>.</li></ul>');}
-		message = paste(message,'</li>');
-		};
-	if (diameter <= 8){
-		message = paste(message,'<li>Since diameter =< 8 mm, see section <a href=\'https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3749714/#__sec30title\'>5.0 Solid Nodules Measuring ≤ 8 mm in Diameter</a> of ACCP guidelines for more specific suggestions</li>');
+covid19positivedeviance <- function(fips = 0, sizetolerance = 0, densitytolerance = 0, growthratemethod = 'nlr'){
+
+x <- getURL("https://raw.githubusercontent.com/qitools/COVID-19-mitigation-with-positive-deviance/master/data/data.counties.final.csv")
+data.counties.final <- read.csv(text = x)
+county.index <- data.counties.final[data.counties.final$fips == fips,]
+
+message = '';
+
+# Filter by density
+data.deviants <- data.counties <- data.counties.final[abs(data.counties.final$density/county.index$density -1) < 0.25,]
+
+# Filter by absolute population (this is V2)
+data.deviants.eligible <- data.deviants[abs(data.deviants$population/county.index$population -1) < 0.25,]
+
+# Sort
+data.deviants.eligible <- data.deviants.eligible[with(data.deviants.eligible, order(Growth.rate.reg)), ]
+
+# Keep deviants and index
+data.deviants <- data.deviants.eligible[c(1:5,which(data.deviants.eligible$fips ==fips),(nrow(data.deviants.eligible)-4):nrow(data.deviants.eligible)),]
+
+	message = paste(message,'<div>data.counties.final: ',nrow(data.counties.final),'</div>');
+	message = paste(message,'<div>eligible deviants: ',nrow(data.deviants.eligible),'</div>');
+	message = paste(message,'<table><tr><th>County</th><th>State</th><th>FIPS</th><th>cases</th><th>days</th><th>Growth rate (%)</th><th>Growth rate (%)<br/>(by nonlinear regression)</th><th>Doubling.time</th><th>tests<br/>(Not available)</th><th>testing.rate<br/>(Not available)</th></tr>');
+
+i <- 1
+style <- 'white'
+for (i in 1:nrow(data.deviants)){
+	if (data.deviants$fips[i] == fips){
+		backgroundcolor <- 'lightgreen'
+		}else{
+		backgroundcolor <- 'white'
 		}
-	#Finish assembling message
-	message = paste(message, '</ul>');
-	message = paste(message, '<div>');
-	if (geo_region == "KS"){message = paste(message,'Regional guidance adapted from national guidelines by ... . ');}
-	message = paste(message, 'Additional detail in guidelines referenced below.</div>');
-  list(message = message)
-#Swensen SJ, Silverstein MD, Ilstrup DM, Schleck CD, Edell ES: The probability of malignancy in solitary pulmonary nodules. Application to small radiologically indeterminate nodules. Arch Intern Med 157. (8): 849-855.1997;
-# Herder GJ, van Tinteren H, Golding RP, et al: Clinical prediction model to characterize pulmonary nodules: validation and added value of 18F-fluorodeoxyglucose positron emission tomography. Chest 128. (4): 2490-2496.2005; Full Text 
+	message = paste(message,'<tr style=\"background-color:',backgroundcolor,'\"><td><a target=_blank href=\"https://en.wikipedia.org/wiki/',data.deviants$county[i],'_County,_',data.deviants$state[i],'\">',data.deviants$county[i],'</a></td><td>',data.deviants$state[i],'</td><td>',data.deviants$fips[i],'</td><td>',data.deviants$cases[i],'</td><td>',data.deviants$days[i],'</td><td>',round(100*(data.deviants$Growth.rate[i]-1),1),'</td><td>',round(100*(data.deviants$Growth.rate.reg[i]-1),1),'</td><td>',round(data.deviants$Doubling.time[i],1),'</td><td>',data.deviants$tests[i],'</td><td>',data.deviants$testing.rate[i],'</td></tr>');
+	}
+	message = paste(message,'</table>');
+	list(message = message)
 }
