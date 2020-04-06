6fe4980e3f286d037d2cf51ec040f84067d16fd8
Robert Badgett <rbadgett@kumc.edu>
2020-04-05 23:21:55 -0500

    Added CountryStartDate

--- R/covid19positivedeviance.R
+++ R/covid19positivedeviance.R
@@ -35,7 +35,7 @@ data.deviants <- data.deviants.eligible[c(1:5,which(data.deviants.eligible$fips 
 	if (nrow(data.deviants.eligible) > 10){
 		message = paste(message,'<div>There are two green rows in your table, because your county was a deviant. Perhaps broadern your criteria.</div>');
 	}
-	message = paste(message,'<table><tr><th>County</th><th>State</th><th>FIPS</th><th>Population<br/>(2018 est)</th><th>Pop density<br/>(2018 est)</th><th>cases</th><th>days</th><th>Growth rate (%)</th><th>Growth rate (%)<br/>(by nonlinear regression)</th><th>Doubling.time</th><th>tests<br/>(Not available)</th><th>testing.rate<br/>(Not available)</th><th>Stay at home<br/>(<a href=\"https://www.nytimes.com/interactive/2020/us/coronavirus-stay-at-home-order.html\" target=_blank>links</a>)</th></tr>');
+	message = paste(message,'<table><tr><th>County</th><th>State</th><th>FIPS</th><th>Population<br/>(2018 est)</th><th>Pop density<br/>(2018 est)</th><th>cases</th><th>Start date</th><th>days</th><th>Growth rate (%)</th><th>Growth rate (%)<br/>(by nonlinear regression)</th><th>Doubling.time</th><th>tests<br/>(Not available)</th><th>testing.rate<br/>(Not available)</th><th>Stay at home<br/>(<a href=\"https://www.nytimes.com/interactive/2020/us/coronavirus-stay-at-home-order.html\" target=_blank>links</a>)</th></tr>');
 
 i <- 1
 style <- 'white'
@@ -45,7 +45,7 @@ for (i in 1:nrow(data.deviants)){
 		}else{
 		backgroundcolor <- 'white'
 		}
-	message = paste(message,'<tr style=\"background-color:',backgroundcolor,'\"><td><a target=_blank href=\"https://en.wikipedia.org/wiki/',data.deviants$county[i],'_County,_',data.deviants$state[i],'\">',data.deviants$county[i],'</a></td><td>',data.deviants$state[i],'</td><td>',data.deviants$fips[i],'</td><td>',data.deviants$population[i],'</td><td>',data.deviants$density[i],'</td><td>',data.deviants$cases[i],'</td><td>',data.deviants$days[i],'</td><td>',round(100*(data.deviants$Growth.rate[i]-1),1),'</td><td>',round(100*(data.deviants$Growth.rate.reg[i]-1),1),'</td><td>',round(data.deviants$Doubling.time[i],1),'</td><td>',data.deviants$tests[i],'</td><td>',data.deviants$testing.rate[i],'</td><td>Comments: imposed by the state<br/>Date: 03/30/2020 Day no: 23<br/><a href=\"https://governor.kansas.gov/governor-kelly-issues-temporary-statewide-stay-home-order-in-ongoing-effort-to-combat-covid-19/\" target=_blank>Link</a> Contributed by jdoe@kumc.edu</td></tr>');
+	message = paste(message,'<tr style=\"background-color:',backgroundcolor,'\"><td><a target=_blank href=\"https://en.wikipedia.org/wiki/',data.deviants$county[i],'_County,_',data.deviants$state[i],'\">',data.deviants$county[i],'</a></td><td>',data.deviants$state[i],'</td><td>',data.deviants$fips[i],'</td><td>',data.deviants$population[i],'</td><td>',data.deviants$density[i],'</td><td>',data.deviants$cases[i],'</td><td>',data.deviants$CountyStartDate[i],'</td><td>',data.deviants$days[i],'</td><td>',round(100*(data.deviants$Growth.rate[i]-1),1),'</td><td>',round(100*(data.deviants$Growth.rate.reg[i]-1),1),'</td><td>',round(data.deviants$Doubling.time[i],1),'</td><td>',data.deviants$tests[i],'</td><td>',data.deviants$testing.rate[i],'</td><td>Comments: imposed by the state<br/>Date: 03/30/2020 Day no: 23<br/><a href=\"https://governor.kansas.gov/governor-kelly-issues-temporary-statewide-stay-home-order-in-ongoing-effort-to-combat-covid-19/\" target=_blank>Link</a> Contributed by jdoe@kumc.edu</td></tr>');
 	}
 	message = paste(message,'</table>');
 	list(message = message)
