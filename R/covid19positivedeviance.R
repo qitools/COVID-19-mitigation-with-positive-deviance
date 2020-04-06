f382fd00c0dfef5c3ecc818e65b78d47b75efff0
Robert Badgett <rbadgett@kumc.edu>
2020-04-06 17:23:55 -0500

    Improved alert logic

--- R/covid19positivedeviance.R
+++ R/covid19positivedeviance.R
@@ -39,8 +39,11 @@ data.deviants <- data.deviants.eligible[c(1:5,which(data.deviants.eligible$fips 
 
 	message = paste(message,'<div>Total counties in database, data.counties.final: ',nrow(data.counties.final),' (excluded counties with < 2 cases or < 14 days since the first case.)</div>');
 	message = paste(message,'<div>Eligible deviant counties: ',nrow(data.deviants.eligible),'</div>');
-	if (nrow(data.deviants.eligible) > 10){
-		message = paste(message,'<div>There are two green rows in your table, because your county was a deviant. Perhaps broadern your criteria.</div>');
+	if (nrow(data.deviants.eligible) < 20){
+	  message = paste(message,'<div><span style=\"font-weight:bold;color:red\">Alert</span>: Less than 20 elibible counties for comparison. Consider expanding your population and density criteria.</div>');
+	}
+	if (nrow(data.deviants) > 10){
+		message = paste(message,'<div><span style=\"font-weight:bold;color:red\">Alert</span>: There are two green rows in your table because your county was a deviant. Perhaps broadern your criteria.</div>');
 	}
 	message = paste(message,'<table><tr><th>County</th><th>State</th><th>FIPS</th><th>Population<br/>(2018 est)</th><th>Pop density<br/>(2018 est)</th><th>cases</th><th>Start date</th><th>days</th><th>Growth rate (%)</th><th>Growth rate (%)<br/>(by nonlinear regression)</th><th>Doubling.time</th><th>tests<br/>(Not available)</th><th>testing.rate<br/>(Not available)</th><th>Stay at home<br/>(<a href=\"https://www.nytimes.com/interactive/2020/us/coronavirus-stay-at-home-order.html\" target=_blank>links</a>)</th></tr>');
 
